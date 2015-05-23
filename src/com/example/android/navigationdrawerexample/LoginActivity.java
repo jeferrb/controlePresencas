8c6ee8d740172e302b11771cffb52a8611ff9159
Jeferson <jeferrb@hotmail.com>
2015-05-22 23:51:03 -0300

    First commit

---
+++ src/com/example/android/navigationdrawerexample/LoginActivity.java
@@ -0,0 +1,82 @@
+package com.example.android.navigationdrawerexample;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.app.AlertDialog;
+
+public class LoginActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+		if (savedInstanceState == null) {
+			getFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.login, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_login,
+					container, false);
+			return rootView;
+		}
+	}
+	
+	public void login(View view){
+		Intent intent = new Intent(view.getContext(), MainActivity.class);
+		intent.putExtra("professor", false);
+		startActivity(intent);
+	}
+	@SuppressWarnings("deprecation")
+	public void recuperaSenha(View view){
+		AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+		alertDialog.setTitle("Falha de rede");
+		alertDialog.setMessage("Por favor verifique a conexão e tente novamente");
+		alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+				// here you can add functions
+			}
+		});
+		//alertDialog.setIcon(R.drawable.icon);
+		alertDialog.show();
+		
+	}
+}
